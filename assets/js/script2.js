d11ce3916246ef47d42489e2c021a20c4769a452
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-01-12 21:39:20 -0500

    added in accuweather api

--- assets/js/script2.js
+++
@@ -1,5 +0,0 @@
-var recipeElements = document.getElementsByClassName("searchedRecipes");
-console.log("Recipe Elements: ", recipeElements);
-
-var recipeArray = Array.from(recipeElements);
-console.log("Array from recipe: ", recipeArray);
